9b74db54b9051cee920f1c0212f51d116591ce46
Blade Nelson <powderblock@gmail.com>
2014-10-21 14:44:03 -0400

    Reorder Prints

--- BadTranslator.py
+++ BadTranslator.py
@@ -6,6 +6,7 @@ endText = text
 numberOfTranslations = int(raw_input("Enter the number of times you want to run over the translations: "))
 # List of all supported langauges:
 langs = ["af", "ach", "ak", "am", "ar", "az", "be", "bem", "bg", "bh", "bn", "br", "bs", "ca", "chr", "ckb", "co", "crs", "cs", "cy", "da", "de", "ee", "el", "en", "eo", "es", "es-419", "et", "eu", "fa", "fi", "fo", "fr", "fy", "ga", "gaa", "gd", "gl", "gn", "gu", "ha", "haw", "hi", "hr", "ht", "hu", "hy", "ia", "id", "ig", "is", "it", "iw", "ja", "jw", "ka", "kg", "kk", "km", "kn", "ko", "kri", "ku", "ky", "la", "lg", "ln", "lo", "loz", "lt", "lua", "lv", "mfe", "mg", "mi", "mk", "ml", "mn", "mo", "mr", "ms", "mt", "ne", "nl", "nn", "no", "nso", "ny", "nyn", "oc", "om", "or", "pa", "pcm", "pl", "ps", "pt-BR", "pt-PT", "qu", "rm", "rn", "ro", "ru", "rw", "sd", "sh", "si", "sk", "sl", "sn", "so", "sq", "sr", "sr-ME", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "tt", "tum", "tw", "ug", "uk", "ur", "uz", "vi", "wo", "xh", "xx-bork", "xx-elmer", "xx-hacker", "xx-klingon", "xx-pirate", "yi", "yo", "zh-CN", "zh-TW","zu"]
+print startText
 for i in range(0, numberOfTranslations):
     # Language (element number in langs) we are going to use for translations
     lang = random.randint(0, len(langs))
@@ -21,5 +22,5 @@ for i in range(0, numberOfTranslations):
     translation2 = translator2.translate(translation.encode('utf-8'))
     # Print what the translation is at final run:
     endText = translation2
-    print translation2
+print endText
 print(str(numberOfTranslations)+" translations later gives us: '"+endText+"' From: '"+startText+"'")
